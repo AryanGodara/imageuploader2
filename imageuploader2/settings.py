851b672e5c9419c6826a9a1845ea6bd66341c72f
Aryan Godara <aryangodara03@gmail.com>
2023-07-22 12:20:19 +0530

    initial commit
    Signed-off-by: Aryan Godara <aryangodara03@gmail.com>

--- imageuploader2/settings.py
+++ imageuploader2/settings.py
@@ -121,7 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'imageuploader2/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
